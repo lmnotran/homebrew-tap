ab82e264bc8aa386f255bb3101cce3c0d9b47e33
Mason Tran <mason.tran@lego.com>
2026-06-29 18:06:17 -0400

    Update bws@2 to 2.1.0

--- Formula/bws@2.rb
+++ Formula/bws@2.rb
@@ -10,21 +10,21 @@ class BwsAT2 < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bitwarden/sdk-sm/releases/download/bws-v2.0.0/bws-aarch64-apple-darwin-2.0.0.zip"
-      sha256 "5bbb43fcec75528c5d78e4dfdb22b6b368ecdff7020bcd853911564587f61f8a"
+      url "https://github.com/bitwarden/sdk-sm/releases/download/bws-v2.1.0/bws-aarch64-apple-darwin-2.1.0.zip"
+      sha256 "9cb1c1c6e6164d83b2e339883ba02b4cbb37188ce9a484b1ce8249443163e066"
     else
-      url "https://github.com/bitwarden/sdk-sm/releases/download/bws-v2.0.0/bws-x86_64-apple-darwin-2.0.0.zip"
-      sha256 "2f33fa7da3d7c3ee1838f3c5f3e8a47051e3fdb01c45701f6844fa0b344e92d1"
+      url "https://github.com/bitwarden/sdk-sm/releases/download/bws-v2.1.0/bws-x86_64-apple-darwin-2.1.0.zip"
+      sha256 "6f626b3971368902af1b9847c02791a1b4666969d7561e2047681cded7997537"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/bitwarden/sdk-sm/releases/download/bws-v2.0.0/bws-aarch64-unknown-linux-gnu-2.0.0.zip"
-      sha256 "49a250d4f3121c67155c195afbad4ced90a92a878c3256ca091276b82e7ad131"
+      url "https://github.com/bitwarden/sdk-sm/releases/download/bws-v2.1.0/bws-aarch64-unknown-linux-gnu-2.1.0.zip"
+      sha256 "18253757286e119d450133a87eb463bf8c1ce418ce24c834f4f250d60cba6f9e"
     else
-      url "https://github.com/bitwarden/sdk-sm/releases/download/bws-v2.0.0/bws-x86_64-unknown-linux-gnu-2.0.0.zip"
-      sha256 "a8340ce01da609200441f2eca0e591173e124f012c88a16afda574279c052013"
+      url "https://github.com/bitwarden/sdk-sm/releases/download/bws-v2.1.0/bws-x86_64-unknown-linux-gnu-2.1.0.zip"
+      sha256 "ba8233c3a4aee5d43e3c73bbd04d99e9bc5aba13bbbfd06d89b073abe732b860"
     end
   end
 
